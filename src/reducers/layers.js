f0052331f19c2dff7f0924eefee8db443e9e9474
Trevor Blades <tdblades@gmail.com>
2016-12-27 18:20:13 -0500

    Unlink children when the parent gets deleted

--- src/reducers/layers.js
+++ src/reducers/layers.js
@@ -1,18 +1,27 @@
 const layerReducer = require('./layer');
+const {linkLayers} = require('../actions');
 
 module.exports = function(state = [], action) {
   switch (action.type) {
     case 'ADD_LAYER':
       return [...state, layerReducer(undefined, action)];
     case 'REMOVE_LAYER':
+      var nextState = state.map(function(layer) {
+        if (layer.parent === action.id) {
+          return layerReducer(layer, linkLayers(layer.id, null));
+        }
+        return layer;
+      });
+
       var index = -1;
-      for (var i = 0; i < state.length; i++) {
-        if (state[i].id === action.id) {
+      for (var i = 0; i < nextState.length; i++) {
+        if (nextState[i].id === action.id) {
           index = i;
           break;
         }
       }
-      return [...state.slice(0, index), ...state.slice(index + 1)];
+
+      return [...nextState.slice(0, index), ...nextState.slice(index + 1)];
     case 'ORDER_LAYERS':
       return action.order.map(id => state.find(layer => layer.id === id));
     case 'SET_LAYER_PROPERTIES':
