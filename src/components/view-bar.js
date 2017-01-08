dc44086be435c090d27e99916e9ca05e4ccf1e7b
Trevor Blades <tdblades@gmail.com>
2017-01-07 18:31:06 -0800

    Make align work with parents

--- src/components/view-bar.js
+++ src/components/view-bar.js
@@ -1,10 +1,14 @@
 const React = require('react');
 const {connect} = require('react-redux');
+const classNames = require('classnames');
 const upperCaseFirst = require('upper-case-first');
 
 const Icon = require('./icon');
 
 const {setLayerProperties} = require('../actions');
+const getParentProperties = require('../util/get-parent-properties');
+const getParents = require('../util/get-parents');
+const layerPropType = require('../util/layer-prop-type');
 
 const alignOptions = {
   x: ['left', 'center', 'right'],
@@ -18,34 +22,43 @@ const ViewBar = React.createClass({
     compositionWidth: React.PropTypes.number.isRequired,
     dispatch: React.PropTypes.func.isRequired,
     getLayerDimensions: React.PropTypes.func.isRequired,
-    layer: React.PropTypes.object,
+    layer: layerPropType,
+    layers: React.PropTypes.arrayOf(layerPropType).isRequired,
     percentPlayed: React.PropTypes.number.isRequired
   },
 
   _onAlignOptionClick: function(properties) {
     const property = Object.keys(properties)[0];
     const dimensions = this.props.getLayerDimensions(this.props.layer.id);
-    const size = property === 'x' ?
+
+    let value = properties[property] * (property === 'x' ?
         this.props.compositionWidth - dimensions.width :
-        this.props.compositionHeight - dimensions.height;
-    let value = properties[property] * size;
-    if (typeof this.props.layer[property] === 'object') {
-      value = Object.assign({}, this.props.layer[property], {
-        [this.props.percentPlayed]: value
-      });
+        this.props.compositionHeight - dimensions.height);
+    if (this.props.layer.parent) {
+      const parents = getParents(this.props.layer, this.props.layers);
+      const parent = getParentProperties(parents, this.props.percentPlayed);
+      const parentScale = parent.scale / this.props.layer.parent.offsetScale;
+      value = (value - parent[property]) / parentScale + this.props.layer.parent[`offset${property.toUpperCase()}`];
     }
+
     this.props.dispatch(setLayerProperties(this.props.layer.id, {
-      [property]: value
+      [property]: typeof this.props.layer[property] === 'object' ?
+          Object.assign({}, this.props.layer[property], {
+            [this.props.percentPlayed]: value
+          }) : value
     }));
   },
 
   render: function() {
+    const alignClassName = classNames('sv-view-bar-align', {
+      'pl-disabled': !this.props.layer
+    });
     return (
       <div className="sv-view-bar">
         <div className="sv-view-bar-zoom">
           100%
         </div>
-        {this.props.layer && <div className="sv-view-bar-align">
+        <div className={alignClassName}>
           {Object.keys(alignOptions).map(axis => {
             const direction = axis === 'x' ? 'horizontal' : 'vertical';
             return (
@@ -58,7 +71,8 @@ const ViewBar = React.createClass({
                   return (
                     <div className="sv-view-bar-align-option"
                         key={position}
-                        onClick={this._onAlignOptionClick.bind(null, {[axis]: index / 2})}
+                        onClick={!this.props.layer ? null :
+                            this._onAlignOptionClick.bind(null, {[axis]: index / 2})}
                         title={index % 2 ?
                             `${upperCaseFirst(position)} ${direction}ly` :
                             `Align ${position}`}>
@@ -69,7 +83,7 @@ const ViewBar = React.createClass({
               </div>
             );
           })}
-        </div>}
+        </div>
       </div>
     );
   }
@@ -78,6 +92,7 @@ const ViewBar = React.createClass({
 module.exports = connect(function(state) {
   return {
     layer: state.layers.present.find(layer => layer.id === state.selectedLayer),
+    layers: state.layers.present,
     percentPlayed: state.percentPlayed
   };
 })(ViewBar);
