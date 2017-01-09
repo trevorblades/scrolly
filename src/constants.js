3f47d5fa84820d60ebf82cf6d79b3d469030dead
Trevor Blades <tdblades@gmail.com>
2017-01-09 08:50:15 -0800

    HTTPS the API URL

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
   API_URL: process.env.API_URL ||
       (process.env.NODE_ENV === 'production' ?
-          'http://api.scrol.ly' : 'http://localhost:8000'),
+          'https://api.scrol.ly' : 'http://localhost:8000'),
   ASSET_DRAG_TYPE: 'text/id',
   FILE_DRAG_TYPE: 'Files'
 };
