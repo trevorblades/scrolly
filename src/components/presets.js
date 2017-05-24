b5bf9c2882e2f9e3b002fcc72a050cf842b5c490
Trevor Blades <tdblades@gmail.com>
2017-05-24 15:46:57 -0700

    Preset panel closes on body click (and add a close button)

--- src/components/presets.js
+++ src/components/presets.js
@@ -1,6 +1,8 @@
 const React = require('react');
+const {findDOMNode} = require('react-dom');
 
 const Button = require('./button');
+const Icon = require('./icon');
 
 const Presets = React.createClass({
   getInitialState: function() {
@@ -9,8 +11,26 @@ const Presets = React.createClass({
     };
   },
 
+  componentWillMount: function() {
+    document.body.addEventListener('click', this._onBodyClick);
+  },
+
+  componentWillUnmount: function() {
+    document.body.removeEventListener('click', this._onBodyClick);
+  },
+
+  _onBodyClick: function(event) {
+    if (!findDOMNode(this).contains(event.target)) {
+      this._closePanel();
+    }
+  },
+
   _onButtonClick: function() {
     this.setState(prevState => ({panelShown: !prevState.panelShown}));
+  },
+
+  _closePanel: function() {
+    this.setState({panelShown: false});
   },
 
   render: function() {
@@ -18,17 +38,22 @@ const Presets = React.createClass({
       <div className="sv-library-presets">
         <Button onClick={this._onButtonClick}>Preset assets</Button>
         {this.state.panelShown && <div className="sv-library-presets-panel">
-          <h5>Preset assets</h5>
+          <h5>
+            <span>Preset assets</span>
+            <a onClick={this._closePanel}>
+              <Icon name="close"/>
+            </a>
+          </h5>
           <div className="sv-library-presets-panel-scroll">
             <div className="sv-library-presets-panel-item">
-              <div className="sv-library-presets-panel-item-asset">
+              <div className="sv-library-presets-panel-item-asset" draggable>
                 <svg fill="none" stroke="black" strokeWidth={2} viewBox="0 0 102 102">
                   <circle cx={51} cy={51} r={50}/>
                 </svg>
               </div>
             </div>
             <div className="sv-library-presets-panel-item">
-              <div className="sv-library-presets-panel-item-asset">
+              <div className="sv-library-presets-panel-item-asset" draggable>
                 <svg fill="none" stroke="black" strokeWidth={2} viewBox="0 0 102 102">
                   <rect height={100} width={100} x={1} y={1}/>
                 </svg>
